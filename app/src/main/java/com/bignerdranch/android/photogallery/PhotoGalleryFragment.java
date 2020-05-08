375131ac3c0a270e845e0847c1328765010b626c
Quizblorg <garyrkirkpatrick@gmail.com>
2020-05-08 15:58:55 +0100

    Page 501

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,20 @@
 package com.bignerdranch.android.photogallery;
 
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -47,18 +51,16 @@ public class PhotoGalleryFragment extends Fragment {
 	}
 
 	private class PhotoHolder extends RecyclerView.ViewHolder {
-		private TextView mTitleTextView;
+		private ImageView mItemImageView;
 
 		public PhotoHolder(@NonNull View itemView) {
 			super(itemView);
-			mTitleTextView = (TextView) itemView;
+			mItemImageView = (ImageView)itemView.findViewById(R.id.item_image_view);
 		}
 
-		public void bindGalleryItem(GalleryItem item) {
-			Log.i(TAG, "bindGalleryItem() called");
-			mTitleTextView.setText(item.toString());
+		public void bindDrawable(Drawable drawable) {
+			mItemImageView.setImageDrawable(drawable);
 		}
-
 	}
 
 	private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
@@ -71,15 +73,16 @@ public class PhotoGalleryFragment extends Fragment {
 
 		@Override
 		public PhotoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-			Log.i(TAG, "onCreateViewHolder() called");
-			TextView textView = new TextView(getActivity());
-			return new PhotoHolder(textView);
+			LayoutInflater inflater = LayoutInflater.from(getActivity());
+			View view = inflater.inflate(R.layout.list_item_gallery, viewGroup, false);
+			return new PhotoHolder(view);
 		}
 
 		@Override
-		public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int i) {
-			GalleryItem galleryItem = mGalleryItems.get(i);
-			photoHolder.bindGalleryItem(galleryItem);
+		public void onBindViewHolder(@NonNull PhotoHolder photoHolder, int position) {
+			GalleryItem galleryItem = mGalleryItems.get(position);
+			Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
+			photoHolder.bindDrawable(placeholder);
 		}
 
 		@Override
